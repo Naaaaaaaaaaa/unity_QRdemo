2d59a4ffe16c570175aac5a5c123fad7a6fc06bd
CGzhao <1341674064@qq.com>
2019-01-21 16:45:09 +0800

    *上传工程

---
+++ QRDemo/Assets/ToLittle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToLittle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		string s = "781A926BCC960F3170868B635536F25177DA4888";
+		Debug.Log(s.ToLower());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
